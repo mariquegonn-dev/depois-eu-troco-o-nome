9087fb732d69fd0d67ae2d28d6b47ced50e1d396
mariquegonn-dev <ocatodev@gmail.com>
2024-06-16 19:59:17 -0300

    fix: fix double equals descriptions

--- src/config/about-me.config.tsx
+++ src/config/about-me.config.tsx
@@ -13,17 +13,6 @@ export const aboutMeConfig = [
   {
     description: (
       <>
-        Sou desenvolvedor frontend, especialista em <strong>React</strong> e{" "}
-        <strong>Typescript</strong>. Aspirante a Fullstack e desenvolvedor de
-        jogos.<br></br>Atualmente estou estudando Analise e Desenvolvimento de
-        Sistemas · 3/5 semestres · porém já atuo na área de desenvolvimento há
-        dois anos.
-      </>
-    ),
-  },
-  {
-    description: (
-      <>
         Amante de trilhas sonoras, ficção científica e pinturas. Pai de três
         gatinhas lindas: Lulu, Kat e Ges.
         <br></br>
